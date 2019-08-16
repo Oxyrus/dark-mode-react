b50a4b1e202477e9f5bd8669d399abe4279d6bb1
Oxyrus <Oxyrus@users.noreply.github.com>
2019-08-16 17:57:25 -0500

    Implement functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  /**
+   * Determine if the checkbox should be checked base on the content
+   * of the localStorage item "theme"
+   */
+  const [checked, setChecked] = useState(localStorage.getItem("theme") === "dark" ? true : false);
+
+  /**
+   * Everytime checked changes, update the property data-theme in the
+   * HTML so it uses the theme we have in localStorage
+   */
+  useEffect(() => {
+    document
+      .getElementsByTagName("HTML")[0]
+      .setAttribute("data-theme", localStorage.getItem("theme"));
+  }, [checked]);
+
+  /**
+   * Update the state of checked and the content of our theme object
+   * in localStorage based on the checkbox toggle
+   */
+  const toggleThemeChange = () => {
+    if (checked === false) {
+      localStorage.setItem("theme", "dark");
+      
+      setChecked(true);
+    } else {
+      localStorage.setItem("theme", "light");
+
+      setChecked(false);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Click the switch to toggle themes</p>
+        <label>
+          <input
+            type="checkbox"
+            defaultChecked={checked}
+            onChange={() => toggleThemeChange()}
+          />
+        </label>
       </header>
     </div>
   );
